d8a4040a2d6a41cc05fb4680c8848818bf56464e
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-12-14 05:55:09 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,10 @@
+/* Print Hello, World. on the first line.
+   Print Hello, Java. on the second line. */
+
+public class print {
+
+    public static void main(String args[]) {
+       System.out.println("Hello, World.\nHello, Java.");
+    }
+}
+
